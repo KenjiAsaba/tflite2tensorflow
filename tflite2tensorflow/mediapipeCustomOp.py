233d51ece7eac0014c44342b4b4ddb84d815c4f9
Kenji Asaba <kasa@akiya-souken.co.jp>
2022-03-27 20:18:48 +0900

    remove unnecessary min/max

--- tflite2tensorflow/mediapipeCustomOp.py
+++ tflite2tensorflow/mediapipeCustomOp.py
@@ -3,6 +3,7 @@
 import tensorflow.compat.v1 as tf
 import numpy as np
 
+#Affine transform points
 def TransformLandmarks(operator, custom_options, tensors, interpreter, landmarks2d, mat):
     # get args
     #landmarks2d = tensors[operator['inputs'][0]] #float32 [b,80,2] landmarks 2d
@@ -19,8 +20,7 @@ def TransformLandmarks(operator, custom_options, tensors, interpreter, landmarks
     landmarks2d_transformed = tf.add(landmarks2d_transformed, translation) #[b,80,2]
     return landmarks2d_transformed
 
-#TODO test
-#Affine transform tensor. Interpolate values by bilinear.
+#Affine transform images using bilinear interpolation
 def TransformTensorBilinear(operator, custom_options, tensors, interpreter, features, mat):
     # get args
     #features = tensors[operator['inputs'][0]] #float32 [b,48,48,32] feature maps
@@ -36,14 +36,6 @@ def TransformTensorBilinear(operator, custom_options, tensors, interpreter, feat
     translation = mat[:,0:2,3] #[b,2]
     translation = tf.expand_dims(translation, axis=1) #[b,1,2]
     translation = tf.expand_dims(translation, axis=1) #[b,1,1,2]
-
-    # construct const tensors
-    ones = tf.ones([b, h, w], dtype=tf.int32) #[b,h,w]
-    zeros = tf.zeros([b, h, w], dtype=tf.int32) #[b,h,w]
-    one_zeros = tf.stack([ones, zeros], axis=3) #[b,h,w,2] = [[[1,0], [1,0] ...
-    zero_ones = tf.stack([zeros, ones], axis=3) #[b,h,w,2] = [[[0,1], [0,1] ...
-    one_ones = tf.ones([b, h, w, 2], dtype=tf.int32) #[b,h,w,2] = [[[1,1], [1,1] ...
-    zero_zeros = tf.zeros([b, h, w, 2], dtype=tf.int32) #[b,h,w,2] = [[[0,0], [0,0] ...
 
     # construct output image coordinates
     # out_coord = [[[ 0,0],[ 0,1],[ 0,2],...,[0,15]],
@@ -76,25 +68,21 @@ def TransformTensorBilinear(operator, custom_options, tensors, interpreter, feat
     weight_floor = tf.expand_dims(weight_floor, axis=3) #[b,h,w,1]
 
     # Find nearest 4 points. 
-    in_coord_floor = tf.cast(in_coord_floor, dtype=tf.int32)
-    in_coord_ceilX = tf.add(in_coord_floor, one_zeros) #[b,h,w,2]
-    in_coord_ceilY = tf.add(in_coord_floor, zero_ones) #[b,h,w,2]
-    in_coord_ceil_ = tf.add(in_coord_floor, one_ones) #[b,h,w,2]
     # Make sure they are in the input image
-    in_coord_floor = tf.minimum(in_coord_floor, [[[[input_w, input_h]]]]) #[b,h,w,2]
-    in_coord_ceilX = tf.minimum(in_coord_ceilX, [[[[input_w, input_h]]]]) #[b,h,w,2]
-    in_coord_ceilY = tf.minimum(in_coord_ceilY, [[[[input_w, input_h]]]]) #[b,h,w,2]
-    in_coord_ceil_ = tf.minimum(in_coord_ceil_, [[[[input_w, input_h]]]]) #[b,h,w,2]
-    in_coord_floor = tf.maximum(in_coord_floor, zero_zeros) #[b,h,w,2]
-    in_coord_ceilX = tf.maximum(in_coord_ceilX, zero_zeros) #[b,h,w,2]
-    in_coord_ceilY = tf.maximum(in_coord_ceilY, zero_zeros) #[b,h,w,2]
-    in_coord_ceil_ = tf.maximum(in_coord_ceil_, zero_zeros) #[b,h,w,2]
+    in_coord_floor = tf.cast(in_coord_floor, dtype=tf.int32) #[b,h,w,XY]
+    in_coord_floor = tf.maximum(in_coord_floor, tf.zeros([b, h, w, 2], dtype=tf.int32)) #[b,h,w,XY]
+    in_coord_floor = tf.minimum(in_coord_floor, [[[[input_w, input_h]]]]) #[b,h,w,XY]
+
+    in_coord_ceil_ = tf.add(in_coord_floor, tf.ones([b, h, w, 2], dtype=tf.int32)) #[b,h,w,XY]
+    in_coord_ceil_ = tf.minimum(in_coord_ceil_, [[[[input_w, input_h]]]]) #[b,h,w,XY]
+
+    in_coord_ceilX = tf.stack([in_coord_floor[:,:,:,1], in_coord_ceil_[:,:,:,0]], axis=3) #[b,h,w,YX] YX for BHWC 
+    in_coord_ceilY = tf.stack([in_coord_ceil_[:,:,:,1], in_coord_floor[:,:,:,0]], axis=3) #[b,h,w,YX]
+
+    in_coord_floor = tf.stack([in_coord_floor[:,:,:,1], in_coord_floor[:,:,:,0]], axis=3) #[b,h,w,YX]
+    in_coord_ceil_ = tf.stack([in_coord_ceil_[:,:,:,1], in_coord_ceil_[:,:,:,0]], axis=3) #[b,h,w,YX]
 
     # calc final pixel value
-    in_coord_floor = tf.stack([in_coord_floor[:,:,:,1], in_coord_floor[:,:,:,0]], axis=3)
-    in_coord_ceilX = tf.stack([in_coord_ceilX[:,:,:,1], in_coord_ceilX[:,:,:,0]], axis=3)
-    in_coord_ceilY = tf.stack([in_coord_ceilY[:,:,:,1], in_coord_ceilY[:,:,:,0]], axis=3)
-    in_coord_ceil_ = tf.stack([in_coord_ceil_[:,:,:,1], in_coord_ceil_[:,:,:,0]], axis=3)
     value_floor = tf.gather_nd(params=features, indices=in_coord_floor, batch_dims=1) #[b,h,w,32]
     value_ceilX = tf.gather_nd(params=features, indices=in_coord_ceilX, batch_dims=1) #[b,h,w,32]
     value_ceilY = tf.gather_nd(params=features, indices=in_coord_ceilY, batch_dims=1) #[b,h,w,32]
@@ -112,16 +100,16 @@ def TransformTensorBilinear(operator, custom_options, tensors, interpreter, feat
 
     return value
 
-#TODO test
 # Left indexとRight indexで指定されたLandmarkを結ぶ線が水平になり、Subset indicesで指定されたLandmrakをちょうど含むような範囲をcropするように、元の画像をAffine変換する行列
 # の逆行列を求める。なぜ、逆行列かといういうと、後の計算で使うのが逆行列だから。
+# Calc inverse of the matrix which represetns the affine transform which crops the area which covers all the landmarks specified by "subset indices" and rotates so that the landmarks specified by "Left index" and "Right index" are horizontally aligned. 
 def Landmarks2TransformMatrix(operator, custom_options, tensors, interpreter, landmarks3d):
     #landmarks3d = tensors[operator['inputs'][0]] #float32 [b,468,3] landmarks
     landmarks2d = landmarks3d[:,:,0:2] # [b,468,2]
     b = landmarks3d.shape[0]
 
     ######################################
-    # get rotation
+    # calc rotation
     ######################################
     rot90_t = tf.constant([[  0.0,  1.0],
                            [ -1.0,  0.0]]) #[2,2], already transposed
@@ -137,7 +125,6 @@ def Landmarks2TransformMatrix(operator, custom_options, tensors, interpreter, la
     u = tf.divide(delta, length) #[b,2] = [[ dx, dy]]
     v = tf.matmul(u, rot90_t)    #[b,2] = [[-dy, dx]]
 
-    # return values
     # mat_rot_inv = [[ dx,  dy],
     #                [-dy,  dx]]
     # mat_rot     = [[ dx, -dy],
@@ -148,23 +135,24 @@ def Landmarks2TransformMatrix(operator, custom_options, tensors, interpreter, la
     mat_rot = tf.transpose(mat_rot_inv, perm=[0,2,1]) #[b,2,2] 切り取り前の画像座標から、切り取り後の画像座標への回転
 
     ######################################
-    # get crop size and center
+    # calc crop size and center
     ######################################
     subset_idxs = custom_options['subset_idxs'] #[80]
     landmarks2d_subset = tf.gather(landmarks2d, indices=subset_idxs, axis=1) #[b,80,2]
-    landmarks2d_subset_rotated = tf.matmul(landmarks2d_subset, mat_rot) #[b,80,2] 切り取り前の画像上でのLandmark座標を、切り取り後の画像上でのLandmark座標に変換
+    landmarks2d_subset_rotated = tf.matmul(landmarks2d_subset, mat_rot) #[b,80,2] 切り取り前の画像上でのLandmark座標を、切り取り後の画像上での向きにあわせて回転
     landmarks2d_subset_rotated_min = tf.reduce_min(landmarks2d_subset_rotated, axis=1) #[b,2]
     landmarks2d_subset_rotated_max = tf.reduce_max(landmarks2d_subset_rotated, axis=1) #[b,2]
 
-    # return values
     crop_size = tf.subtract(landmarks2d_subset_rotated_max, landmarks2d_subset_rotated_min) #[b,2], max - min
     center = tf.multiply(tf.add(landmarks2d_subset_rotated_min, landmarks2d_subset_rotated_max), tf.constant(0.5)) #[b,2], 1/2 * (max + min)
     center = tf.expand_dims(center, axis=1) #[b,1,2]
-    center = tf.matmul(center, mat_rot_inv) #[b,1,2] 切り取り後の画像座標から、切り取り前の画像座標に変換
+    center = tf.matmul(center, mat_rot_inv) #[b,1,2] 切り取り後の画像上での向きから、切り取り前の画像上での向きに回転
 
     ######################################
-    # get mat
+    # calc scale
     ######################################
+    # s = [[scale_x * crop_size.x / output_w],
+    #      [scale_y * crop_size.y / output_h]]]
     output_w = custom_options['output_width']
     output_h = custom_options['output_height']
     scale_x = custom_options['scale_x']
@@ -173,18 +161,21 @@ def Landmarks2TransformMatrix(operator, custom_options, tensors, interpreter, la
     scaling_const_y = scale_y / output_h
     scaling_const = tf.constant([[scaling_const_x, scaling_const_y]]) #[1,2]
     scale = tf.multiply(scaling_const, crop_size) #[b,2]
-    #scale = tf.expand_dims(scale, axis=1) #[b,1,2] 
 
+    ######################################
+    # calc translation and final mat
+    ######################################
     # mat = [[ sx*dx, -sy*dy, 0, tx],
     #        [ sx*dy,  sy*dx, 0, ty]]
+    # where
+    #
+    # t = center - shift
+    #
+    # shift = -0.5 * output_w * sx * u 
+    #       + -0.5 * output_h * sy * v
     sxu = tf.multiply(u, scale[:,0]) #[b,1,2]
     syv = tf.multiply(v, scale[:,1]) #[b,1,2]
     zeros = tf.zeros([b, 1, 2])
-
-    #shift_scaling_const = tf.constant([[scale_x * 0.5, scale_y * 0.5]]) #[1,2]
-    #shift_scale = tf.multiply(shift_scaling_const, crop_size) #[b,2]
-    #shift_scale = tf.expand_dims(shift_scale, axis=1) #[b,1,2]
-    #translation = tf.subtract(center, shift) #[b,1,2]
 
     shift_u = tf.multiply(sxu, output_w * 0.5) #[b,1,2]
     shift_v = tf.multiply(syv, output_h * 0.5) #[b,1,2]
@@ -202,23 +193,3 @@ def Landmarks2TransformMatrix(operator, custom_options, tensors, interpreter, la
                                     [0.0, 0.0, 0.0, 1.0]]]), [b,1,1]) #[b,2,4]
     mat = tf.concat([mat, unit_zw], axis=1) #[b,4,4]
     return mat
-
-    #
-    # u = tf.squeeze(u) #[2]
-    # v = tf.squeeze(v) #[2]
-    # mat_rot_inv = tf.stack([u, v]) #[2,2]
-
-    # = [[sx, sy],
-    #    [sx, sy]]
-    # mat_scale_elementWise = tf.concat([scale, scale], axis=1) #[b,2,2]
-
-    # mat = [[ sx*dx, -sy*dy],
-    #        [ sx*dy,  sy*dx]]
-    # mat = tf.multiply(mat_rot, mat_scale_elementWise)
-    # tf.multiply(scale, u), tf.multiply(scale, v), 
-
-    # mat = [[ sx*dx, -sy*dy, 0, minX],
-    #        [ sx*dy,  sy*dx, 0, minY]]
-    # mat = tf.transpose(mat)
-    #mat = tf.concat([mat, tf.constant([[0.0], [0.0]]), translation], axis=0) #[4,2]
-    # mat = tf.transpose(mat) #[2,4]
